91b8d9a0ddd1e7ae30e1269880258578a08ad68a
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-11 11:54:05 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -227,7 +227,7 @@ elif menu == "Visualización de datos":
         # Lectura del archivo csv de prevalencia de anemia para país y continente
         data_country = pd.read_csv("data/world_bank_continentes.csv")
 
-        col1, col2, col3 = st.columns([1.15, 0.1, 1.65])
+        col1, col2, col3 = st.columns([1.2, 0.1, 1.7])
 
         with col1:
             st.subheader("Veamos la situación de la anemia infantil en cada año 🌍👀")
