23375bbf2c04b4f0c0fba8ff1ea7e41af4fecac8
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-12 09:36:29 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -438,8 +438,8 @@ elif menu == "Visualización de datos":
                 font=dict(color="white", family="Arial"),  # Estilo tipográfico (limpio)
             )
             # Mostrar cuadrícula para facilitar el debug
-            fig.update_xaxes(zeroline=True, showgrid=True)
-            fig.update_yaxes(zeroline=True, showgrid=True)
+            fig.update_xaxes(range=[0, 1], zeroline=False, showgrid=False)  # Fijar rango del eje X
+            fig.update_yaxes(range=[0, 1], zeroline=False, showgrid=False)  # Fijar rango del eje Y
             return fig
 
 
