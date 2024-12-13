512d0928436e92a9331dac601e6a5b230036b993
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-13 01:07:05 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -18,6 +18,20 @@ import numpy as np
 st.set_page_config(page_title="Análisis Global de la Anemia", layout="wide")
 
 # Colocar el logo de la universidad en la parte superior
+# Inyectar CSS para centrar la imagen
+st.markdown(
+    """
+    <style>
+    .sidebar .element-container {
+        display: flex;
+        justify-content: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Mostrar la imagen centrada
 st.sidebar.image("imagenes/escudo-unalm.png", use_container_width=False, width=200)
 # Agregar texto en markdown con un estilo de fuente distintivo
 st.sidebar.markdown(
