7a947c9bdc3a35b81868da7b42a95aa2338c9237
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-13 02:32:48 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -176,7 +176,7 @@ if menu == "Introducción":
         )
 
     with col2:
-        st.image("images/anemia.jpg", use_container_width=True)
+        st.image("imagenes/anemia.jpg", use_container_width=True)
 
 
     st.subheader("¿Cómo se ha vivido la anemia alrededor del mundo?")
