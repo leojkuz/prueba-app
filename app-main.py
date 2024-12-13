95b32a731eb18910c7a5e6ce4f5f391890ad1db2
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-12 22:56:17 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -291,7 +291,7 @@ elif menu == "Visualización de datos":
             st.subheader("Prevalencia histórica de anemia por niveles de ingresos")
             st.markdown("Los datos muestran una diferencia en los niveles de anemia infantil según el nivel de ingresos promedio de los países")
 
-        col1, col2 = st.columns([1.7, 1.3])
+        col1, col2 = st.columns([1.5, 1.5])
 
         with col1:
             # Cargar los datos desde el archivo CSV
