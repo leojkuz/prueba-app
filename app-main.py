fcf8d0cf2fc97d38ed353f7997eb1fb980860795
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-13 00:13:24 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -1553,7 +1553,7 @@ elif menu == "Visualización de datos":
             )
         ])
         fig_yes.update_layout(
-            title=dict(text='Anemia en consumidores de hierro', x=0.1, font=dict(size=16)),
+            title=dict(text='Anemia en consumidores de hierro', x=0.25, font=dict(size=16)),
             showlegend=False
         )
 
