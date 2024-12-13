5f26388e7091cddfbaf4d0b10f6e9c1738d10dc0
Leonardo Ccorahua Madera <leonardocm1112@gmail.com>
2024-12-12 20:39:26 -0500

    Update app-main.py

--- app-main.py
+++ app-main.py
@@ -1408,8 +1408,6 @@ elif menu == "Visualización de datos":
                 xanchor="center",
                 x=0.5
             ),
-            plot_bgcolor='white',
-            template="simple_white",
             margin=dict(t=50, b=80)
         )
 
